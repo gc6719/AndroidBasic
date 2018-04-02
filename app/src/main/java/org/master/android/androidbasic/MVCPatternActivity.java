85bf67abe4f7e78144b50a45dbb091106e2e3bd1
gurucm <gc6719@gmail.com>
2018-04-02 19:06:01 +0530

    Modified  And Added Testadd file

--- app/src/main/java/org/master/android/androidbasic/MVCPatternActivity.java
+++ app/src/main/java/org/master/android/androidbasic/MVCPatternActivity.java
@@ -9,5 +9,6 @@ public class MVCPatternActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvcpattern);
+        System.out.println("****");
     }
 }
---
+++ app/src/main/java/org/master/android/androidbasic/Testaddfile.java
@@ -0,0 +1,8 @@
+package org.master.android.androidbasic;
+
+/**
+ * Created by Gururaj.C.M on 02-04-2018
+ */
+
+public class Testaddfile {
+}
